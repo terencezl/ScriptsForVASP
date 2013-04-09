4ff7c9056236998148b65e5cb8382edf7daf76e3
terencelz <Terence@Terences-MacBook-Air.local>
2013-04-08 23:49:36 -0400

    better for abs

--- display.sh
+++ display.sh
@@ -14,15 +14,15 @@ if [[ $1 == "entest" ||  $1 == "kptest" ]]; then
     fi
     for ((n=$2; n<=$3; n=n+$4))
     do
-        E_LC1=$(grep sigma $n/OUTCAR | tail -1 | cut -c 68-)				# get the total energy when LC1
-        E_LC2=$(grep sigma $n-1/OUTCAR | tail -1 | cut -c 68-)				# when LC2
-        DE=$(echo "($E_LC2)-($E_LC1)" | bc)						# echoing the expr into bc to get DE
-        if [ $n == $2 ]; then DE_pre=$DE; fi						# the first cycle let dDE = 0
-        dDE=$(echo "($DE)-($DE_pre)" | bc)						# get dDE between two adjacent rows
-#       if (($(echo $dDE/0.00001 | bc) < 0)); then dDE=$(echo "-($dDE)" | bc); fi	# return the int value to compare with 0, return the abs value of dDE
-        dDE=$(echo "a=$dDE;if(0>a)a*=-1;a" | bc)                # return the absolute value of dDE
-        echo "$n   $E_LC1   $E_LC2   $DE   $dDE" >> $fname				# output into a file
-        DE_pre=$DE									# set DE_pre for the next cycle to get dDE
+        E_LC1=$(grep sigma $n/OUTCAR | tail -1 | cut -c 68-)        # get the total energy when LC1
+        E_LC2=$(grep sigma $n-1/OUTCAR | tail -1 | cut -c 68-)      # when LC2
+        DE=$(echo "($E_LC2)-($E_LC1)" | bc)                         # echoing the expr into bc to get DE
+        if [ $n == $2 ]; then DE_pre=$DE; fi                        # the first cycle let dDE = 0
+        dDE=$(echo "($DE)-($DE_pre)" | bc)                          # get dDE between two adjacent rows
+#       dDE=$(echo "a=$dDE;if(0>a)a*=-1;a" | bc)                    # return the absolute value of dDE
+        dDE=${dDE#-}                                                # return the absolute value of dDE
+        echo "$n   $E_LC1   $E_LC2   $DE   $dDE" >> $fname          # output into a file
+        DE_pre=$DE                                                  # set DE_pre for the next cycle to get dDE
     done
 elif [ $1 == "lctest" ]; then
     fname="lctest_output.txt"
@@ -30,11 +30,11 @@ elif [ $1 == "lctest" ]; then
     echo "LC(Ams)   E" > $fname
     List=""
     for n in $(awk "BEGIN{for(i=$2;i<=$3;i+=$4)print i}")
-    do i=$(echo "scale=2;$n/1" | bc)			# change decimal format from 5.1 to 5.10
+    do i=$(echo "scale=2;$n/1" | bc)                                # change decimal format from 5.1 to 5.10
     List=$List" "$i
     done
     for n in $List
-    do echo "$n   $(grep sigma $n/OUTCAR | tail -1 | cut -c 68-)" >> $fname; done	# output into a file
+    do echo "$n   $(grep sigma $n/OUTCAR | tail -1 | cut -c 68-)" >> $fname; done   # output into a file
 else
     echo "Specify what you are going to test! entest/kptest/lctest"
 fi
