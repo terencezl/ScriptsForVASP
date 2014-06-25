31bba7a78b4aba7a6318c17e1b4af07e82c32fee
terencezl <terencelz90@gmail.com>
2014-06-25 17:22:29 -0400

    Bug fix. Added initial argument checking.

--- Prepare.sh
+++ Prepare.sh
@@ -53,68 +53,74 @@ function change_dir_name_with_hyphen {
 
 function submission_trigger {
     if [[ $is_submit ]]; then
-        echo -e '    \c'
+        echo -e '        \c'
         qsub qsub.parallel
     fi
 }
 
+function argparse {
+    echo -e "Creating test directory \'$directory_name\'..."
+    if [[ -d $directory_name && "$(ls -A $directory_name)" ]]; then
+        echo "  Directory contains files or sub-directories."
+    fi
+    mkdir "$directory_name" 2> /dev/null
+    cd "$directory_name"
+    echo "  Preparing $test_type..."
+    fname="$test_type"_output.txt
+    shift 1
+
+    while getopts ":s:e:n:i:c:y:r:mf" opt; do
+        case $opt in
+        s)
+            start=$OPTARG
+            ;;
+        e)
+            end=$OPTARG
+            ;;
+        n)
+            num_points=$OPTARG
+            ;;
+        i)
+            interval=$OPTARG
+            ;;
+        c)
+            scaling_const=$OPTARG
+            ;;
+        y)
+            cryst_sys=$OPTARG
+            ;;
+        r)
+            ions_rotator_args=$OPTARG
+            ;;
+        m)
+            is_submit=true
+            echo "  -m triggered job submission."
+            ;;
+        f)
+            is_override=true
+            echo "  -f triggered overriding existing subdirectories."
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+      esac
+    done
+}
 
 directory_name="$1"
-echo "Creating test directory \'$directory_name\'..."
-if [[ -d $directory_name && "$(ls -A $directory_name)" ]]; then
-    echo "  Directory contains files or sub-directories."
-fi
-mkdir "$directory_name" 2> /dev/null
-cd "$directory_name"
 test_type="${directory_name%%_*}"
-echo "  Preparing $test_type..."
-fname="$test_type"_output.txt
-shift 1
-
-while getopts ":s:e:n:i:c:y:r:mo" opt; do
-    case $opt in
-    s)
-        start=$OPTARG
-        ;;
-    e)
-        end=$OPTARG
-        ;;
-    n)
-        num_points=$OPTARG
-        ;;
-    i)
-        interval=$OPTARG
-        ;;
-    c)
-        scaling_const=$OPTARG
-        ;;
-    y)
-        cryst_sys=$OPTARG
-        ;;
-    r)
-        ions_rotator_args=$OPTARG
-        ;;
-    m)
-        is_submit=true
-        echo "-m triggered job submission."
-        ;;
-    o)
-        is_override=true
-        echo "-o triggered overriding existing subdirectories."
-        ;;
-    \?)
-        echo "Invalid option: -$OPTARG" >&2
-        exit 1
-        ;;
-    :)
-        echo "Option -$OPTARG requires an argument." >&2
-        exit 1
-        ;;
-  esac
-done
-
 
 if [[ $test_type == "entest" || $test_type == "kptest" ]]; then
+    argparse
+    if [[ -z $start || -z $end || -z $interval || -z $scaling_const ]]; then
+        echo " -s -e -i -c should be all set with valid values!" >&2
+        exit 1
+    fi
     lc1=$scaling_const
     lc2=$(echo "print($lc1+0.1)" | python)
     for ((dir=$start; dir<=$end; dir=$dir+$interval))
@@ -139,6 +145,11 @@ if [[ $test_type == "entest" || $test_type == "kptest" ]]; then
     done
 
 elif [[ $test_type == "lctest" ]]; then
+    argparse
+    if [[ -z $start || -z $end || -z $num_points ]]; then
+        echo " -s -e -n should be all set with valid values!" >&2
+        exit 1
+    fi
     dir_list=$(echo -e "import numpy as np\nfor i in np.linspace($start,$end,$num_points): print('{0:.3f}'.format(i))" | python)
     for dir in $dir_list
     do
@@ -150,6 +161,11 @@ elif [[ $test_type == "lctest" ]]; then
     done
 
 elif [[ $test_type == "rttest" ]]; then
+    argparse
+    if [[ -z $start || -z $end || -z $num_points ]]; then
+        echo " -s -e -n should be all set with valid values!" >&2
+        exit 1
+    fi
     dir_list=$(echo -e "import numpy as np\nfor i in np.linspace($start,$end,$num_points): print('{0:.3f}'.format(i))" | python)
     dir_list=$(change_dir_name_with_hyphen "$dir_list")
     for dir in $dir_list
@@ -163,6 +179,11 @@ elif [[ $test_type == "rttest" ]]; then
     done
 
 elif [[ $test_type == "agltest" ]]; then
+    argparse
+    if [[ -z $start || -z $end || -z $num_points || -z $ions_rotator_args ]]; then
+        echo " -s -e -n -r should be all set with valid values!" >&2
+        exit 1
+    fi
     dir_list=$(echo -e "import numpy as np\nfor i in np.linspace($start,$end,$num_points): print('{0:.3f}'.format(i))" | python)
     dir_list=$(change_dir_name_with_hyphen "$dir_list")
     for dir in $dir_list
@@ -176,6 +197,7 @@ elif [[ $test_type == "agltest" ]]; then
     done
 
 elif [[ $test_type == *c[1-9][1-9]* ]]; then
+    argparse
     if [ $test_type == c44 ]; then
         dir_list="0.020 0.035 0.050n"
     elif [ $test_type == c11-c12 ]; then
@@ -193,7 +215,5 @@ elif [[ $test_type == *c[1-9][1-9]* ]]; then
 
 else
     echo "Specify what you are going to test!" >&2
-    cd ..
-    rm -r "$directory_name"
     exit 1
 fi
